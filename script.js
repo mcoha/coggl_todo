fc484c7dfd148991ec95a97b981ed856f6e2cec9
mcoha <coha.marko@gmail.com>
2014-10-15 19:31:25 +0200

    Added date to the list

--- script.js
+++ script.js
@@ -4,12 +4,13 @@ $(document).ready(function(){
 // Glavna funckija//
 // Klik miša
     $('#button').click(function(){
+
+      var datum =$('#datepicker').val();
      
        if ($('input').val() !== '') {
-        console.log("datum je : ",datum);
        	var toAdd=$('input[name=checkListItem]').val();
-     
-       $(".list").append('<div class="item">' + toAdd +'<input class="check" type="checkbox" ></input>'+ '</div>');
+        console.log("datum je",datum);
+       $(".list").append('<div class="item">' + toAdd + datum +'<input class="check" type="checkbox" ></input>'+  '</div>');
         p+=1;}
         $('input').val('');
     return false;
@@ -17,10 +18,12 @@ $(document).ready(function(){
     
     $('form').submit(function () {
 // Enter na formu
+
 if ($('input').val() !== '') {
+  var datum =$('#datepicker').val();
     	var toAdd=$('input[name=checkListItem]').val();
      
-        $(".list").append('<div class="item">' + toAdd +'<input class="check" type="checkbox" ></input>'+ '</div>');
+        $(".list").append('<div class="item">' + toAdd + "   " +datum +'<input class="check" type="checkbox" ></input>'+ '</div>');
         p+=1;
         
         }
@@ -60,7 +63,9 @@ $('#button3').click(function() {
     cleanup();
 
 });
-var datum = $( "#datepicker" ).datepicker().value("getDate" );
-$( "#datepicker" ).datepicker("option", "dateFormat", "dd.mm.yy");
+
+$( "#datepicker" ).datepicker();
+$( "#datepicker" ).datepicker("option", "dateFormat", "dd.mm.yy.");
+
 });
     
