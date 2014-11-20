fb3dc6aec3014188618aabf9e51fa58cefd1ee41
mcoha <coha.marko@gmail.com>
2014-11-20 16:48:32 +0100

    Added sql-ite db

--- notes/tasks/models.py
+++ notes/tasks/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Tasks(models.Model):
+    note = models.CharField(max_length=400)
+    due_date = models.DateTimeField('date due')
+	
